0ff06c25a79d04de52b713521cc6d8ba449d6a94
Olivier Panico <panico@lac10.epfl.ch>
2024-04-05 17:41:32 +0200

    small modif

--- diag_tcv/dischargeInfoMdsObject.py
+++ diag_tcv/dischargeInfoMdsObject.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 #DBS
 # from DBS.beamtracing.src.tcv.io import retrieve_plasma_data #for density and mag eq
-from DBS import definitions as defs
+# from DBS import definitions as defs
 
 #dataAnalysis
 from dataAnalysis.utils.plot_utils import prep_multiple_subplots, plot_1d, plot_2d, get_cmap_list
@@ -170,8 +170,9 @@ class TCVShot():
     # NOTE: Not all data is available from any Lac#, connect to standard LAC to be shure
     # data = MDSplus.Data.execute(query)
     
-    def __init__(self, shot):
+    def __init__(self, shot, verbose=False):
         self.shot = shot
+        self.verbose = verbose
 
         print('')
         print('Opening shot ', shot)
@@ -649,6 +650,8 @@ class TCVShot():
         else:
             self.get_thomson_fit()
             plot_thomson_time(shot, rho_list, tmin, tmax, rho_avg)
+
+            
     
     def plot_heating(self):
         self.get_nbi()
@@ -659,12 +662,12 @@ class TCVShot():
         
         if self.tag_nbi:
             if self.tag_nb1:
-                ax.plot(a.nbi_time, a.nb1*1e3, color='red', label='NB1')
+                ax.plot(self.nbi_time, self.nb1*1e3, color='red', label='NB1')
             if self.tag_nb2:
-                ax.plot(a.nbi_time, a.nb2*1e3, color='green', label='NB2')
+                ax.plot(self.nbi_time, self.nb2*1e3, color='green', label='NB2')
         
         if self.tag_ecrh:
-            ax.plot(a.ecrh_time, a.ecrh_tot, color='xkcd:dark yellow', label='ECRH tot')
+            ax.plot(self.ecrh_time, self.ecrh_tot, color='xkcd:dark yellow', label='ECRH tot')
         
         ax.set_ylabel('Power [kW]')
         ax.set_xlabel('Time [s]')
@@ -672,6 +675,7 @@ class TCVShot():
         
         
             
+#%% Function to store
 
 def plot_Te_time(shot, rho_list, tmin=0, tmax=-1):
     dthomson = get_thomson_data(shot=shot)
@@ -806,99 +810,3 @@ def plot_Ti_time(shot, rho_list, remove_zeros=True):
 
 
 
-#%%
-### Reference shot for mix ECRH / NBI
-# a=TCVShot(80257)
-
-
-### Mix ECRH / NBI 
-## USN
-a=TCVShot(80322) #pas de cxrs
-# a=TCVShot(80338)
-## LSN
-#a=TCVShot(80335) #Disrupted at 0.4s
-# a=TCVShot(80336)  #Data available but disrupts at 1.2s
-
-
-### Mix NB1 / NB2
-# a=TCVShot(80324) #good 
-# a=TCVShot(80328) #good
-
-### Density ramp
-# a=TCVShot(80376) #Some nans but overall ok ?
-
-a.get_cxrs_fit(set_nan_to_zero=False)
-a.get_thomson_fit()
-
-rho_plot = 0.9
-
-thrho09 = get_closest_ind(a.th_rho, rho_plot)
-cxrsrho09 = get_closest_ind(a.cxrs_rho, rho_plot)
-
-fig, ax = plot_1d(a.th_te[thrho09,:], a.th_time,marker='+', color='blue', label=r'$T_e ; \rho = ${:.3f}'.format(a.th_rho[thrho09]), grid=True)
-ax.fill_between(a.th_time, a.th_te[thrho09,:]- a.th_te_err[thrho09,:], a.th_te[thrho09,:]+a.th_te_err[thrho09,:], color='blue', alpha=0.2)
-ax.plot(a.cxrs_time[:], a.cxrs_ti[:,cxrsrho09],marker='x', color='red', label=r'$T_i ; \rho = ${:.3f}'.format(a.cxrs_rho[cxrsrho09]))
-ax.fill_between(a.cxrs_time[:], a.cxrs_ti[:,cxrsrho09]-a.cxrs_ti_err[:,cxrsrho09], a.cxrs_ti[:,cxrsrho09]+a.cxrs_ti_err[:,cxrsrho09], alpha=0.2, color='red')
-ax.set_title('#{}'.format(a.shot))
-ax.legend()
-ax.set_xlabel('time [s]')
-ax.set_ylabel(r'$T_i, T_e$')
-# ax.axvline(0.9, color='green')
-
-# ax.axvline(1.1, color='black')
-# ax.axvline(1.3, color='black')
-# ax.axvline(1.5, color='green')
-# ax.axvline(1.7, color='black')
-
-#%%
-
-
-'''
-Has been replaced by results from  raytracing because could not get  array for mag eq 
-def get_mag_eq_data(shot):
-    
-    with tcv.shot(shot) as dis:
-        #The magnetic seems to not be accessible in python
-        # key = r'\magnetics::rbphi' 
-        # rbphi = dis.tdi(tcv_eq("bzero","fbte"))
-
-        key = r'\results::q_95'
-        q95 = dis.tdi(key)
-    
-        key = r'\results::r_contour'
-        r_contour = dis.tdi(key)
-
-    return q95, r_contour
-'''
-
-from DBS.beamtracing.src.tcv.io import _retrieve_plasma_data
-def deprecated_get_ne_B_from_raytracing(shot,time):
-
-    plasma = _retrieve_plasma_data(shot=shot, time=time, verbose=False, outpath=None)
-    
-    Btor_tot = plasma.BtotStruct.B.phi
-    
-    R_B = plasma.BtotStruct.coord.R  # 1d array
-    nr, nz = np.shape(Btor_tot)
-    B0 = abs(np.mean(Btor_tot[nr//2, :]))
-
-    return Btor_tot
-    
-    #position of LCFS: r_contour
-    # rsep = np.mean(r_contour[0]) #For now an avg on the whole shot but should be taken at the right time
-    # ind_sep = get_closest_ind(R_B, rsep)
-    # Btorsep = abs(np.mean(Btor_tot[ind_sep, :]))
-
-    ne = plasma.neStruct.ne
-    rho_psi_ne = plasma.neStruct.rho_psi
-
-
-    q_psi = plasma.eq.q_psi
-    # psi_values = plasma.eq.psi_values
-    # psi_axis = plasma.eq.psi_axis
-    psi_LCFS = 0
-    psi_normalized = (plasma.eq.psi_values - plasma.eq.psi_axis) / (psi_LCFS - plasma.eq.psi_axis)
-    rho_psi = np.sqrt(abs(psi_normalized))
-
-
-    return B0, ne, rho_psi_ne, q_psi, rho_psi
